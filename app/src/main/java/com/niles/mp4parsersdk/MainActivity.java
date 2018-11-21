0071c899095a740589f67abc07243a397c7720b0
Niles <niulinguo@163.com>
2018-11-21 11:36:48 +0800

    v1.1 Release

--- app/src/main/java/com/niles/mp4parsersdk/MainActivity.java
+++ app/src/main/java/com/niles/mp4parsersdk/MainActivity.java
@@ -2,15 +2,21 @@ package com.niles.mp4parsersdk;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.niles.mp4parser.Mp4Info;
 import com.niles.mp4parser.Mp4Utils;
 
 import java.io.File;
@@ -27,21 +33,46 @@ public class MainActivity extends AppCompatActivity {
     private static final String VIDEO_PATH = "hobot/media/adas/video";
     private static final Pattern PATTERN = Pattern.compile("^ADAS_RAW_Nebula_([0-9]{8}-[0-9]{6}_[0-9]{3})\\.mp4$");
     private final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd-HHmmss_SSS", Locale.getDefault());
+    private ImageView mImageView;
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bitmap bitmap = (Bitmap) msg.obj;
+            mImageView.setImageBitmap(bitmap);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mImageView = findViewById(R.id.iv_image);
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                 || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
-        } else {
-            onPermissionGranted();
         }
     }
 
-    private void onPermissionGranted() {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION: {
+                for (int grantResult : grantResults) {
+                    if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                        break;
+                    }
+                }
+                break;
+            }
+            default: {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
+    }
+
+    public void onButtonClicked(View view) {
         File dir = new File(Environment.getExternalStorageDirectory(), VIDEO_PATH);
         if (!dir.exists()) {
             Toast.makeText(this, dir.getAbsolutePath() + "不存在", Toast.LENGTH_SHORT).show();
@@ -55,40 +86,29 @@ public class MainActivity extends AppCompatActivity {
                 String time = matcher.group(1);
                 try {
                     Date date = DATE_FORMAT.parse(time);
-//                    Mp4Info mp4Info = Mp4Utils.getMp4Info(file.getAbsolutePath());
-//                    if (mp4Info != null) {
-//                        int duration = mp4Info.getDuration();
-//                        if (duration > 10000) {
-                    Log.e("crop_file", file.getAbsolutePath());
-                    File outFile = new File(dir, file.getName() + "crop.mp4");
-                    if (outFile.exists()) {
-                        outFile.delete();
+                    Mp4Info mp4Info = Mp4Utils.getMp4Info(file.getAbsolutePath());
+                    if (mp4Info != null) {
+                        Log.e("crop_file", file.getAbsolutePath());
+                        File outFile = new File(dir, file.getName() + "crop.mp4");
+                        if (outFile.exists()) {
+                            outFile.delete();
+                        }
+                        outFile.createNewFile();
+//                        Mp4Utils.crop(file.getAbsolutePath(), 0, 10, outFile.getAbsolutePath());
+                        for (int i = 0; i < 10; i++) {
+                            Bitmap snap = Mp4Utils.snap(file.getAbsolutePath(), i);
+                            if (snap != null) {
+                                Message message = Message.obtain();
+                                message.what = 0;
+                                message.obj = snap;
+                                mHandler.sendMessageDelayed(message, i * 1000);
+                            }
+                        }
+                        return;
                     }
-                    outFile.createNewFile();
-                    Mp4Utils.crop(file.getAbsolutePath(), 0, 10, outFile.getAbsolutePath());
-//                        }
-//                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_PERMISSION: {
-                for (int grantResult : grantResults) {
-                    if (grantResult != PackageManager.PERMISSION_GRANTED) {
-                        break;
-                    }
-                }
-                onPermissionGranted();
-                break;
-            }
-            default: {
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
             }
         }
     }
--- mp4parser/src/main/java/com/niles/mp4parser/Mp4Utils.java
+++ mp4parser/src/main/java/com/niles/mp4parser/Mp4Utils.java
@@ -1,5 +1,6 @@
 package com.niles.mp4parser;
 
+import android.graphics.Bitmap;
 import android.media.MediaMetadataRetriever;
 import android.text.TextUtils;
 import android.util.Log;
@@ -23,10 +24,15 @@ import java.util.Locale;
  */
 public class Mp4Utils {
 
+    /**
+     * 获取 MP4 文件信息
+     */
     public static Mp4Info getMp4Info(String path) {
+        // 解析文件
         MediaMetadataRetriever retriever = new MediaMetadataRetriever();
         retriever.setDataSource(path);
 
+        // 获取参数
         String duration = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
         String width = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH);
         String height = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT);
@@ -36,6 +42,7 @@ public class Mp4Utils {
 
         Mp4Info mp4Info = null;
 
+        // 如果 mp4 文件不完整，将获取不到信息
         if (!TextUtils.isEmpty(duration)
                 && !"Null".equalsIgnoreCase(duration)) {
             mp4Info = new Mp4Info.Builder()
@@ -50,9 +57,38 @@ public class Mp4Utils {
         return mp4Info;
     }
 
-    public static boolean crop(String path, long fromTime, long toTime, String outPath) {
+    /**
+     * 获取视频的某一帧图片
+     *
+     * @param path mp4文件路径
+     * @param time 截图时间，单位秒
+     * @return 图片
+     */
+    public static Bitmap snap(String path, int time) {
+        // 解析文件
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(path);
+
+        Bitmap frame = retriever.getFrameAtTime(time * 1000 * 1000, MediaMetadataRetriever.OPTION_CLOSEST);
+
+        retriever.release();
+
+        return frame;
+    }
+
+    /**
+     * 裁剪一段视频
+     *
+     * @param path     mp4文件路径
+     * @param fromTime 开始剪裁时间，单位秒
+     * @param toTime   结束剪裁时间，单位秒
+     * @param outPath  裁剪后mp4文件
+     * @return 是否剪裁成功
+     */
+    public static boolean crop(String path, int fromTime, int toTime, String outPath) {
         try {
 
+            // 获取 videoTrack
             Movie movie = MovieCreator.build(path);
             Track videoTrack = null;
             for (Track track : movie.getTracks()) {
@@ -61,10 +97,17 @@ public class Mp4Utils {
                 }
             }
 
+            // 没有 videoTrack，则返回失败
             if (videoTrack == null) {
                 return false;
             }
 
+            /*
+            循环遍历，计算需要的视频是从哪一帧到哪一帧
+            currTime：当前的时间，单位秒
+            fromSample：起始帧
+            toSample：结束帧
+             */
             long[] sampleDurations = videoTrack.getSampleDurations();
             double currTime = 0;
             int currSample = 0;
@@ -72,21 +115,26 @@ public class Mp4Utils {
             int toSample = 0;
             for (long duration : sampleDurations) {
 
+                // 定位起始帧
                 if (currTime <= fromTime) {
                     fromSample = currSample;
                 }
 
+                // 定位结束帧
                 if (currTime <= toTime) {
                     toSample = currSample;
                 }
 
+                // 累加当前时间和当前帧
                 currTime += 1.0 * duration / videoTrack.getTrackMetaData().getTimescale();
                 currSample += 1;
             }
 
+            // 裁剪视频
             Movie resultMovie = new Movie();
             resultMovie.addTrack(new AppendTrack(new CroppedTrack(videoTrack, fromSample, toSample)));
 
+            // 保存
             Container container = new DefaultMp4Builder().build(resultMovie);
             FileOutputStream outputStream = new FileOutputStream(new File(outPath));
             container.writeContainer(outputStream.getChannel());
